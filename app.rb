27261d4f06aed5dc23f0bd2f2beb2240247661b7
Hanna Slesarska <127249050+Hanna-Slesarska@users.noreply.github.com>
2024-05-19 03:54:12 +0000

    initial

--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
 require "sinatra"
 require "sinatra/reloader"
 
+require "http"
+
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  
+  response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}")
+
+  erb(:home)
 end
